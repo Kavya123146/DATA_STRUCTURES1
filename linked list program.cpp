3ba2116b6904d88e64c6f3c2ed714c4274020556
Kavya123146 <allisravani77@gmail.com>
2026-04-09 21:09:02 +0530

    Add files via upload

---
+++ linked list program.cpp
@@ -0,0 +1,89 @@
+//linked list program
+#include<stdio.h>
+#include<stdlib.h>
+struct stack
+{
+	int data;
+	struct stack *next;
+};
+struct stack *top=0,*temp=0;
+void push()
+{
+	struct stack *newnode=0;
+	newnode=(struct stack *)malloc(sizeof(struct stack));
+	printf("\nenter data:");
+	scanf("%d",&newnode->data);
+	newnode->next=top;
+	top=newnode;
+}
+void pop()
+{
+	if(top==0)
+	{
+		printf("\nunderflow");
+	}
+	else
+	{
+		temp=top;
+		top=top->next;
+		printf("\nThe poped element is:%d",temp->data);
+		free(temp);
+	}
+}
+void peek()
+{
+	if(top==0)
+	{
+		printf("\nstack is empty");
+	}
+	else
+	{
+		printf("\n The top most element is:%d",top->data);
+	}
+}
+void display()
+{
+	if(top==0)
+	{
+		printf("\nunderflow");
+	}
+	else
+	{
+		temp=top;
+		while(temp!=0)
+		{
+			printf("\n%d",temp->data);
+			temp=temp->next;
+		}
+	}
+}
+int main()
+{
+	int choice,ch=1;
+	while(ch)
+	{
+		printf("\n1.push()\n2.pop()\n3.peek()\n4.display()");
+		printf("\nenter your choice");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+				push();
+				break;
+			case 2:
+				pop();
+				break;
+			case 3:
+				peek();
+				break;
+			case 4:
+				display();
+				break;
+			default:
+				printf("\nInvalid choice");
+		}
+		printf("\nDo you want another option:");
+		scanf("\n%d",&ch);
+	}
+	return 0;
+}
