a9909cc7125bd51a491c02dfdb0ea5c62653cd71
Kavya123146 <allisravani77@gmail.com>
2026-04-17 22:18:41 +0530

    Add files via upload

--- queues implementation program.cpp
+++ queues implementation program.cpp
@@ -1,106 +1,127 @@
 //queues implementation program by using arrays
 #include<stdio.h>
 #define MAX 5
+
 int queue[MAX];
-int rear=-1;
-int front=-1;
+int rear = -1;
+int front = -1;
+
 void enqueue()
 {
-	int a;
-	if(rear==MAX-1)
-	{
-		printf("\noverflow");
-	}
-	else
-	{
-		printf("\nenter element:");
-		scanf("%d",&a);
-		if(rear==-1&&front==-1)
-		{
-			rear=front=0;
-			queue[rear]=a;
-		}
-		else
-		{
-			rear++;
-			queue[rear]=a;
-			
-		}
-	}
+    int a;
+    if(rear == MAX - 1)
+    {
+        printf("\nOverflow");
+    }
+    else
+    {
+        printf("\nEnter element: ");
+        scanf("%d", &a);
+
+        if(front == -1 && rear == -1)
+        {
+            front = rear = 0;
+        }
+        else
+        {
+            rear++;
+        }
+
+        queue[rear] = a;
+    }
 }
+
 void dequeue()
 {
-	int temp;
-	if(front==-1)
-	{
-		printf("\nunderflow");
-	}
-	else if(rear==front)
-	{
-		temp=queue[front];
-		printf("\nThe dequeued element %d",temp);
-		rear=front=-1;
-	}
-	else
-	{
-		temp=queue[front];
-		printf("\nThe dequeued element=%d",temp);
-		front++;
-	}
+    int temp;
+
+    if(front == -1)
+    {
+        printf("\nUnderflow");
+    }
+    else if(front == rear)
+    {
+        temp = queue[front];
+        printf("\nThe dequeued element = %d", temp);
+        front = rear = -1;
+    }
+    else
+    {
+        temp = queue[front];
+        printf("\nThe dequeued element = %d", temp);
+        front++;
+    }
 }
+
 void peek()
 {
-	if(front==-1)
-	{
-		printf("\nunderflow");
-	}
-	else
-	{
-		printf("\nThe front element=%d",queue[front]);
-	}
+    if(front == -1)
+    {
+        printf("\nUnderflow");
+    }
+    else
+    {
+        printf("\nThe front element = %d", queue[front]);
+    }
 }
+
 void display()
 {
-	int i;
-	if(front==-1)
-	{
-		printf("\nunderflow");
-	}
-	else
-	{
-		for(i=front;i<=rear;i++)
-		{
-			printf("\n%d",queue[i]);
-		}
-	}
+    int i;
+
+    if(front == -1)
+    {
+        printf("\nQueue is empty");
+    }
+    else
+    {
+        printf("\nQueue elements are:\n");
+        for(i = front; i <= rear; i++)
+        {
+            printf("%d ", queue[i]);
+        }
+    }
 }
+
 int main()
 {
-	int choice,ch=1;
-	while(ch)
-	{
-		printf("\n1.enqueue()\n2.dequeue()\n3.peek()\n4.display()");
-		printf("\nenter your choice");
-		scanf("%d",&choice);
-		switch(choice)
-		{
-			case1:
-				enqueue();
-				break;
-			case2:
-				dequeue();
-				break;
-			case3:
-				peek();
-				break;
-			case4:
-				display();
-				break;
-			default:
-				printf("\nInvalid option");		
-		}
-		printf("\nDo you want another option:");
-		scanf("%d",&ch);
-	}
-	return 0;
+    int choice, ch = 1;
+
+    while(ch)
+    {
+        printf("\n\n--- Queue Operations ---");
+        printf("\n1. Enqueue");
+        printf("\n2. Dequeue");
+        printf("\n3. Peek");
+        printf("\n4. Display");
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+                enqueue();
+                break;
+
+            case 2:
+                dequeue();
+                break;
+
+            case 3:
+                peek();
+                break;
+
+            case 4:
+                display();
+                break;
+
+            default:
+                printf("\nInvalid choice");
+        }
+
+        printf("\nDo you want to another option(yes:1||no:0): ");
+        scanf("%d", &ch);
+    }
+
+    return 0;
 }
